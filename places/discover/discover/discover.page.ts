242cb764532960baae25706bc451d4ec23ab3b00
Brian Muturi <brianmuturi2@gmail.com>
2021-10-06 17:25:52 +0300

    booking app: added side drawer

--- places/discover/discover/discover.page.ts
+++ places/discover/discover/discover.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {PlacesService} from '../../places/services/places.service';
 import {Place} from '../../places/models/place.model';
 import { Pipe } from '@angular/core';
+import {MenuController} from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -12,10 +13,15 @@ export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService,
+              private menuController: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+  }
+
+  openMenu() {
+    this.menuController.toggle();
   }
 
 }
