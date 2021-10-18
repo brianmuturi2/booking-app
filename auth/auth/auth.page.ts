0ea0348a7e04c7987b8d4f1faa4d117951cb1626
Brian Muturi <brianmuturi2@gmail.com>
2021-10-18 16:54:17 +0300

    booking-app: updated auth form

--- auth/auth/auth.page.ts
+++ auth/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +10,8 @@ import {LoadingController} from '@ionic/angular';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+
+  isLogin = true;
 
   constructor(private authService: AuthService,
               private router: Router,
@@ -21,9 +24,9 @@ export class AuthPage implements OnInit {
     this.authService.logIn();
 
     this.loadingController.create({
+      duration: 5500,
       keyboardClose: true,
       message: 'Login you in...',
-      duration: 5500
     }).then(res => {
       res.present();
       res.dismiss();
@@ -35,6 +38,27 @@ export class AuthPage implements OnInit {
 
   logout() {
     this.authService.logOut();
+  }
+
+  toggleisLogin() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    let email = '';
+    let password = '';
+    if (!form.valid) {
+      return;
+    } else {
+      email = form.value.email;
+      password = form.value.password;
+    }
+
+    if (this.isLogin) {
+      // send request to login
+    } else {
+      // send request to sign up
+    }
   }
 
 }
