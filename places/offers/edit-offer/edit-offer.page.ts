1403b024c3940e0636c49d11be9de579b0be74ce
Brian Muturi <brianmuturi2@gmail.com>
2021-10-21 18:38:44 +0300

    booking app: added edit offer form

--- places/offers/edit-offer/edit-offer.page.ts
+++ places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PlacesService} from '../../places/services/places.service';
+import {Place} from '../../places/models/place.model';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  placeId: string;
+
+  place: Place;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private placesService: PlacesService,
+              private navController: NavController) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(res => {
+      if (!res.has('placeId')) {
+        this.navController.navigateBack('/places/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(res.get('placeId'));
+
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        })
+      });
+    });
   }
 
 }
