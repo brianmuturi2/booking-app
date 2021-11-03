229a98371ae93395d8275736a892c84ce27e9621
brianmuturi2 <brianmuturi2@gmail.com>
2021-11-03 19:32:40 +0300

    booking-app: updated bookings to subject observable

--- bookings/booking/bookings.page.ts
+++ bookings/booking/bookings.page.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {BookingService} from '../services/booking.service';
 import {Booking} from '../models/booking.model';
 import {IonItemSliding} from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
 
+  bookingsSub: Subscription;
   loadedBookings: Booking[];
   constructor(private bookingService: BookingService) { }
 
   ngOnInit() {
-    this.loadedBookings = this.bookingService.bookings;
+    this.bookingsSub = this.bookingService.bookings.subscribe(res => this.loadedBookings = res);
   }
 
   cancelBooking(id, slide: IonItemSliding) {
     slide.close();
 
     // cancel booking with id
+  }
+
+  ngOnDestroy() {
+    if (this.bookingsSub) {
+      this.bookingsSub.unsubscribe();
+    }
   }
 
 }
--- bookings/services/booking.service.ts
+++ bookings/services/booking.service.ts
@@ -1,17 +1,42 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import {Booking} from '../models/booking.model';
+import {AuthService} from "../../auth/services/auth.service";
+import {delay, take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
-  private _bookings: Booking[] = [
-    new Booking('xyz', 'p1', 'abc', 'Manhattan Mansion', 2)
-  ];
+  private _bookings = new BehaviorSubject<Booking[]>([
+    new Booking(
+        'xyz',
+        'p1',
+        'abc',
+        'Manhattan Mansion',
+        'http://www.themostexpensivehomes.com/wp-content/uploads/2013/11/The-most-expensive-homes-Woolworth-Mansion-in-New-York-City.jpg',
+        'Woolworth',
+        'mansion',
+        15,
+        new Date(),
+        new Date()
+    )
+  ]);
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   get bookings() {
-    return [...this._bookings];
+    return this._bookings.asObservable();
+  }
+
+  addBooking(placeId: string, placeTitle: string, placeImage: string, firstName: string, lastName: string, guestNumber: number, dateFrom: Date, dateTo: Date) {
+    const newBooking = new Booking(Math.random().toString(), placeId, this.authService.userId, placeTitle, placeImage, firstName, lastName, guestNumber, dateFrom, dateTo);
+    return this._bookings.pipe(take(1), delay(1000), tap(res => {
+      this._bookings.next(res.concat(newBooking));
+    }));
+  }
+
+  cancelBooking(bookingId: string) {
+
   }
 }
