fa8513c7e733a1e355c2b8c0f7c49988478f76ed
brianmuturi2 <brianmuturi2@gmail.com>
2021-11-22 20:48:11 +0300

    booking app: implemented storing & retrieving user id from device storage

--- auth/services/auth.service.ts
+++ auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, from } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import {keys, urls} from 'src/environments/environment';
 import {User} from '../models/user.model';
+import { Storage } from '@capacitor/storage';
 
 interface AuthResData {
   idToken: string;
@@ -39,6 +40,27 @@ export class AuthService {
     this._user.next(null);
   }
 
+  autoLogin() {
+    return from(Storage.get({key: 'user'})).pipe(map(storedData => {
+      if (!storedData || !storedData.value) {
+        return null;
+      }
+      const parsedData = JSON.parse(storedData.value) as {token: string; userId: string; tokenExpirationDate: string; email: string};
+      const expirationTime = new Date(parsedData.tokenExpirationDate);
+      if (expirationTime <= new Date()) {
+        return null;
+      }
+      const user = new User(parsedData.userId, parsedData.email, parsedData.token, expirationTime);
+      return user;
+    }), tap (user => {
+      if (user) {
+        this._user.next(user);
+      }
+    }), map(user => {
+      return !!user;
+    }));
+  }
+
   get isAuthenticated() {
     return this._user.asObservable().pipe(map(user => {
       if (user) {
@@ -58,5 +80,14 @@ export class AuthService {
   private setUserData(userData: AuthResData) {
       const expiryTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
       this._user.next(new User(userData.localId, userData.email, userData.idToken, expiryTime));
+      this.storeAuthData(userData.localId, userData.idToken, userData.email, expiryTime.toISOString());
+  }
+
+  private async storeAuthData(userId: string, token: string, email: string, tokenExpirationDate: string) {
+    const val = JSON.stringify({userId, token, email, tokenExpirationDate});
+    await Storage.set({
+      key: 'user',
+      value: val,
+    });
   }
 }
