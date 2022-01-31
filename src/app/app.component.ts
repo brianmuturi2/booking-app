dbc201cde50fd6e01883570804e550af55e18b65
brianmuturi2 <brianmuturi2@gmail.com>
2022-01-31 11:48:01 +0300

    Booking app: formatted app files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from './auth/services/auth.service';
 import {Router} from '@angular/router';
-import { Subscription } from 'rxjs';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy{
-  private authSub: Subscription;
-  private previousAuthState = false;
+export class AppComponent implements OnInit, OnDestroy {
+    private authSub: Subscription;
+    private previousAuthState = false;
 
-  constructor(private authService: AuthService,
-              private router: Router) {}
-
-  ngOnInit() {
-    this.authSub = this.authService.isAuthenticated.subscribe(isAuth => {
-      if (!isAuth && this.previousAuthState !== isAuth) {
-        this.router.navigateByUrl('/auth');
-      }
-      this.previousAuthState = isAuth;
-    });
-  }
-
-  logOut() {
-    this.authService.logOut();
-    this.router.navigate(['/auth']);
-  }
-
-  ngOnDestroy() {
-    if (this.authSub) {
-      this.authSub.unsubscribe();
+    constructor(private authService: AuthService,
+                private router: Router) {
     }
-  }
+
+    ngOnInit() {
+        this.authSub = this.authService.isAuthenticated.subscribe(isAuth => {
+            if (!isAuth && this.previousAuthState !== isAuth) {
+                this.router.navigateByUrl('/auth');
+            }
+            this.previousAuthState = isAuth;
+        });
+    }
+
+    logOut() {
+        this.authService.logOut();
+        this.router.navigate(['/auth']);
+    }
+
+    ngOnDestroy() {
+        if (this.authSub) {
+            this.authSub.unsubscribe();
+        }
+    }
 }
